9aa39ffd0874722a5628ba76905b1ab97125df61
dicarne <dicarne@zhishudali.ink>
2023-07-26 13:59:16 +0800

    [fix] Added image width style settings to prevent overflowing the page

--- docnote.go
+++ docnote.go
@@ -72,7 +72,7 @@ func renderHook(w io.Writer, node ast.Node, entering bool) (ast.WalkStatus, bool
 				return ast.GoToNext, true
 			}
 			if IsImage(file.Ext) {
-				io.WriteString(w, fmt.Sprintf("<img src=\"%s\" alt=\"%s\"/>", file.ShortWebPath, shortURL(d.URL)))
+				io.WriteString(w, fmt.Sprintf("<img src=\"%s\" alt=\"%s\" style=\"width:100%%\"/>", file.ShortWebPath, shortURL(d.URL)))
 			} else {
 				io.WriteString(w, fmt.Sprintf("<a href=\"%s\"  target=\"_blank\">%s</a>", file.ShortWebPath, shortURL(d.URL)))
 			}
